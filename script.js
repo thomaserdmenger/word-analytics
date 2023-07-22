bfcbf45359139c8a824d29a467fb226f460c239d
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-22 12:18:46 +0200

    Decrement twitter's number

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const input = document.querySelector('.word__input');
 const words = document.querySelector('.number__words');
 const characters = document.querySelector('.number__characters');
+const twitter = document.querySelector('.number__twitter');
 
 // Count words
 const countWords = e => {
@@ -29,3 +30,24 @@ const countCharacters = e => {
 };
 
 input.addEventListener('keyup', countCharacters);
+
+// Count Twitter
+const countTwitter = e => {
+  const inputString = e.target.value;
+  const inputLength = inputString.length;
+  const twitterLength = 280;
+
+  // Decrement twitter's number
+  const twitterCount = twitterLength - inputLength;
+
+  // Display decrementation to screen
+  twitter.textContent = twitterCount;
+
+  if (twitterCount < 0) {
+    twitter.classList.add('word__warning');
+  } else {
+    twitter.classList.remove('word__warning');
+  }
+};
+
+input.addEventListener('keyup', countTwitter);
